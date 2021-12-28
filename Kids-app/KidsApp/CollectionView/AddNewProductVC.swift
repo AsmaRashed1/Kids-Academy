e92a96458c09f853336fa740c2db263bdada2515
AsmaRashed1 <Asma.Rashed.has.s@tuwaiq.edu.sa>
2021-12-28 16:25:54 +0300

    upload image & data

--- Kids-app/KidsApp/CollectionView/AddNewProductVC.swift
+++ Kids-app/KidsApp/CollectionView/AddNewProductVC.swift
@@ -8,22 +8,47 @@
 import UIKit
 import Firebase
 import FirebaseFirestore
+import BSImagePicker
+import Photos
+
 class AddNewProductVC: UIViewController {
 
     @IBOutlet weak var des: UITextField!
     
     @IBOutlet weak var lableDone: UILabel!
     
+    @IBOutlet weak var imageView: UIImageView!
     
+    
+    let BSVC = ImagePickerController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        BSVC.maxNumberOfSelections = 1
     }
     
     @IBAction func uploadImage(_ sender: UIButton) {
         
+        
+
+        presentImagePicker(BSVC, select: { (PhAsset :PHAsset) in
+            // User selected an asset. Do something with it. Perhaps begin processing/upload?
+        }, deselect: { (PhAsset : PHAsset) in
+            // User deselected an asset. Cancel whatever you did when asset was selected.
+        }, cancel: { (PhAsset :[PHAsset]) in
+            // User canceled selection.
+        }, finish: { (PhAsset :[PHAsset]) in
+            
+            
+            if PhAsset.count > 0 {
+             var img = PhAsset[0]
+                
+                self.imageView.image = self.getUIImage(asset: img)
+                
+            }
+            
+        }, completion: nil)
     }
-    
     
     @IBAction func UploadData(_ sender: Any) {
             
@@ -37,6 +62,20 @@ class AddNewProductVC: UIViewController {
     }
     
 
-   
+    func getUIImage(asset: PHAsset) -> UIImage? {
+
+        var img: UIImage?
+        let manager = PHImageManager.default()
+        let options = PHImageRequestOptions()
+        options.version = .original
+        options.isSynchronous = true
+        manager.requestImageDataAndOrientation(for: asset, options: options){ (data, _, _, _) in
+
+            if let data = data {
+                img = UIImage(data: data)
+            }
+        }
+        return img
+    }
 
 }
